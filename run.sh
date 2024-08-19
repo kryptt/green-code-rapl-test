5fbcff108af95df1e5c57bd238c6e312daf413b6
Rodolfo Hansen <kryptt@gmail.com>
2024-08-19 15:21:14 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+# Edit this file to run your test, the workflow we setup will read the output.json file in this 
+# workdir to confirm the result, and will also generate the expected energy consumption automatically for you
+
+sleep 5
